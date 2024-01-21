4e87d23e7f50ed4bb4c07907c3b2f65d121fc44a
TopKop-svg <loookintome@gmail.com>
2024-01-21 17:11:54 +0900

    database connection and implementing class logic UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Alex", "Li", (byte) 23);
+        userDao.saveUser("Den", "Nau", (byte) 26);
+        userDao.saveUser("Zina", "Ali", (byte) 22);
+        userDao.saveUser("Vita", "Golos", (byte) 24);
+
+        //userDao.removeUserById(1);
+        userDao.getAllUsers();
+        //userDao.cleanUsersTable();
+        //userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/usersdb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Aka_two_big_s0da";
+    private static Connection connection;
+        static {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+
 }
